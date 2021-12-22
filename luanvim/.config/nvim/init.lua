c9d9727bb1ec1ae177623de6ff648a7fc2325fbb
woodroww <example528@gmail.com>
2021-12-22 17:44:20 -0500

    added back user.blablabla in init.lua

--- luanvim/.config/nvim/init.lua
+++ luanvim/.config/nvim/init.lua
@@ -1,12 +1,12 @@
 -- This adds in ~/.config/nvim/lua/user/options.lua
 
-require "options"
-require "keymaps"
-require "plugins"
-require "colorscheme"
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
 
-require "cmp_config"
-require "treesitter_playground"
-require "treesyntax"
-require "plug-colorizer"
-require "telescope_config"
+require "user.cmp_config"
+require "user.treesitter_playground"
+require "user.treesyntax"
+require "user.plug-colorizer"
+require "user.telescope_config"
