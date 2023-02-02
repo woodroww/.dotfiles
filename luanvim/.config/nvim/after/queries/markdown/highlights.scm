69713f333eb9af6bfaa4698fce0356eb5d7d77e0
woodroww <example528@gmail.com>
2023-02-02 09:34:44 -0500

    should be with last commit

---
+++ luanvim/.config/nvim/after/queries/markdown/highlights.scm
@@ -0,0 +1,16 @@
+;; extends
+((atx_heading
+  (atx_h1_marker) @hh1
+  (_) @h1))
+((atx_heading
+  (atx_h2_marker) @hh2
+  (_) @h2))
+((atx_heading
+  (atx_h3_marker) @hh3
+  (_) @h3))
+((atx_heading
+  (atx_h4_marker) @hh4
+  (_) @h4))
+((atx_heading
+  (atx_h5_marker) @hh5
+  (_) @h5))
